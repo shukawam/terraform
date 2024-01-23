14ab3107572a71febc68877bb7bc65812b1871dc
shukawam <shukawam@gmail.com>
2024-01-23 11:48:30 +0900

    Add compute and networks

---
+++ oci/terraform.tf
@@ -0,0 +1,23 @@
+provider "oci" {
+    region = var.region
+    tenancy_ocid = var.tenancy_ocid
+    user_ocid = var.user_ocid
+    fingerprint = var.fingerprint
+    private_key = var.private_key
+}
+
+terraform {
+    required_providers {
+      oci = {
+        source = "oracle/oci"
+        version = "< 6.0.0"
+      }
+    }
+    backend "remote" {
+      hostname = "app.terraform.io"
+      organization = "shukawam"
+      workspaces {
+        name = "oci"
+      }
+    }
+}
---
+++ oci/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+
+}
+
+variable "tenancy_ocid" {
+
+}
+
+variable "user_ocid" {
+
+}
+
+variable "fingerprint" {
+
+}
+
+variable "private_key" {
+    sensitive = true
+}
+
+variable "compartment_ocid" {
+
+}
