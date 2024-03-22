c1b1785fc3a8ce46337c3de1e4ac2f4664dd6675
shukawam <shukawam@gmail.com>
2024-03-22 17:03:56 +0900

    Fixed

--- oci/container-instance-with-lb/output.tf
+++ oci/container-instance-with-lb/output.tf
@@ -1,3 +1,3 @@
 output "ci" {
-  value = oci_container_instances_container_instance.shukawam_container_instance.vnics.private_ip
+  value = oci_container_instances_container_instance.shukawam_container_instance.vnics.0.private_ip
 }
