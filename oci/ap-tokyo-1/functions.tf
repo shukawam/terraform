b2a54a6a776040671ae263eefdac02dbc908fbcc
shukawam <shukawam@gmail.com>
2024-03-25 17:54:13 +0900

    Fixed invaliad id

--- oci/ap-tokyo-1/functions.tf
+++ oci/ap-tokyo-1/functions.tf
@@ -7,7 +7,7 @@ resource "oci_functions_application" "application" {
     oci_core_subnet.private_subnet.id
   ]
   trace_config {
-    domain_id  = oci_apm_apm_domain.apm.compartment_id
+    domain_id  = oci_apm_apm_domain.apm.id
     is_enabled = true
   }
   freeform_tags = local.freeform_tags
