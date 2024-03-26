b16aa4237714a72d19ba0bcec7b62f403d6a858f
shukawam <shukawam@gmail.com>
2024-03-26 12:59:53 +0900

    Delete CI w/ LB

--- oci/container-instance-with-lb/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  ad = data.oci_identity_availability_domains.ads.availability_domains[0].name
-}
--- oci/container-instance-with-lb/output.tf
+++
@@ -1,3 +0,0 @@
-output "ci" {
-  value = oci_container_instances_container_instance.shukawam_container_instance.vnics.0.private_ip
-}
