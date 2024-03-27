b35d2feb89237ab2f03e297030e22d5044be485e
shukawam <shukawam@gmail.com>
2024-03-27 15:10:24 +0900

    Fixed syntax error

--- oci/us-chicago-1/redis.tf
+++ oci/us-chicago-1/redis.tf
@@ -1,6 +1,6 @@
 resource "oci_redis_redis_cluster" "redis_cluster" {
   compartment_id     = var.compartment_ocid
-  display_name       = format("%s-redis-cluster")
+  display_name       = format("%s-redis-cluster", var.prefix)
   node_count         = local.redis.node
   node_memory_in_gbs = local.redis.memory
   software_version   = local.redis.version
