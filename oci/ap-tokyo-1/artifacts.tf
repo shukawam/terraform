a1402d19ea915d084b0af9d4b35f540fc37c899b
shukawam <shukawam@gmail.com>
2024-04-06 14:24:49 +0900

    Add container registry

--- oci/ap-tokyo-1/artifacts.tf
+++ oci/ap-tokyo-1/artifacts.tf
@@ -3,3 +3,15 @@ resource "oci_artifacts_container_repository" "get_secret_from_vault" {
   display_name   = "shukawam/fn/get-secret-from-vault"
   freeform_tags  = local.freeform_tags
 }
+
+resource "oci_artifacts_container_repository" "kafka-producer" {
+  compartment_id = var.compartment_ocid
+  display_name   = "shukawam/kafka-producer"
+  freeform_tags  = local.freeform_tags
+}
+
+resource "oci_artifacts_container_repository" "kafka-consumer" {
+  compartment_id = var.compartment_ocid
+  display_name   = "shukawam/kafka-consumer"
+  freeform_tags  = local.freeform_tags
+}
