edc50805b06f530da617627199327681f8617e35
shukawam <shukawam@gmail.com>
2024-04-22 16:22:32 +0900

    Add registry

--- oci/ap-tokyo-1/artifacts.tf
+++ oci/ap-tokyo-1/artifacts.tf
@@ -4,6 +4,12 @@ resource "oci_artifacts_container_repository" "get_secret_from_vault" {
   freeform_tags  = local.freeform_tags
 }
 
+resource "oci_artifacts_container_repository" "micronaut-function" {
+  compartment_id = var.compartment_ocid
+  display_name   = "shukawam/fn/micronaut-function"
+  freeform_tags  = local.freeform_tags
+}
+
 resource "oci_artifacts_container_repository" "kafka-producer" {
   compartment_id = var.compartment_ocid
   display_name   = "shukawam/kafka-producer"
