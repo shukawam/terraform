8d42470d9ea10883f16dc983df9f66f17bbe25f2
shukawam <shukawam@gmail.com>
2024-04-30 11:38:34 +0900

    Added logs for micronaut application

--- oci/ap-tokyo-1/logging.tf
+++ oci/ap-tokyo-1/logging.tf
@@ -3,3 +3,9 @@ resource "oci_logging_log_group" "log_group" {
   display_name = format("%s-log-group", var.prefix)
   freeform_tags = local.freeform_tags
 }
+
+resource "oci_logging_log" "micronaut_logs" {
+  display_name = format("%s-micronaut-logs", var.prefix)
+  log_group_id = oci_logging_log_group.log_group.id
+  log_type = "CUSTOM"
+}
