c51d8e0cc242c1be4fb568b4ea99db8711d0b16d
shukawam <shukawam@gmail.com>
2024-07-31 14:38:47 +0900

    Fixed some statements

--- oci/ap-tokyo-1/notifications.tf
+++ oci/ap-tokyo-1/notifications.tf
@@ -1,6 +1,6 @@
 import {
   id = "ocid1.onstopic.oc1.ap-tokyo-1.aaaaaaaa6gdechfbz57ll27yqybr7gt2gylm6ijfzfuxwhh4xza2ocklvb5a"
-  to = "topic"
+  to = oci_ons_notification_topic.topic
 }
 
 resource "oci_ons_notification_topic" "topic" {
@@ -10,7 +10,7 @@ resource "oci_ons_notification_topic" "topic" {
 
 import {
   id = "ocid1.onssubscription.oc1.ap-tokyo-1.aaaaaaaai3ts5luy2jdwblqxe3wl6pc447iy3ivmoq2odmyoqopwe7ohslhq"
-  to = "slack_subscription"
+  to = oci_ons_subscription.slack_subscription
 }
 
 resource "oci_ons_subscription" "slack_subscription" {
