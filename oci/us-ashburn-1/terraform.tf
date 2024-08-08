6bcf6f10377271576f427e479f7b8088c02140db
shukawam <shukawam@gmail.com>
2024-08-08 13:57:12 +0900

    Added ashburn respirces

---
+++ oci/us-ashburn-1/terraform.tf
@@ -0,0 +1,23 @@
+provider "oci" {
+  region       = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = var.private_key
+}
+
+terraform {
+  required_version = "1.7.5"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "< 6.0.0"
+    }
+  }
+  cloud {
+    organization = "shukawam"
+    workspaces {
+      name = "us-ashburn-1"
+    }
+  }
+}
