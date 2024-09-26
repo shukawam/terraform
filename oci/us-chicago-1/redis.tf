fa1924ea0b891b6923b9e3d298d58b8f6365df69
shukawam <shukawam@gmail.com>
2024-09-26 10:03:08 +0900

    Delete chicago resource

--- oci/us-chicago-1/redis.tf
+++
@@ -1,9 +0,0 @@
-resource "oci_redis_redis_cluster" "redis_cluster" {
-  compartment_id     = var.compartment_ocid
-  display_name       = format("%s-redis-cluster", var.prefix)
-  node_count         = local.redis.node
-  node_memory_in_gbs = local.redis.memory
-  software_version   = local.redis.version
-  subnet_id          = oci_core_subnet.private_subnet.id
-  freeform_tags      = local.freeform_tags
-}
