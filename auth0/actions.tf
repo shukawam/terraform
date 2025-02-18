47ca878b053b306d9fd39b42aa1565ae4047c26c
shukawam <shukawam@gmail.com>
2025-02-18 16:26:32 +0900

    Fixed version value

--- auth0/actions.tf
+++ auth0/actions.tf
@@ -1,9 +1,10 @@
 resource "auth0_action" "add_groups_claim" {
-  name = "Add groups claim to ID token"
-  code = file("./scripts/add-groups-claim-to-id-token.js")
+  name    = "Add groups claim to ID token"
+  code    = file("./scripts/add-groups-claim-to-id-token.js")
   runtime = "node22"
+  deploy  = true
   supported_triggers {
-    id = "post-login"
-    version = "v1"
+    id      = "post-login"
+    version = "Version 1"
   }
 }
