7e7580cfb59dfacd72eb1ca38b8ef47ebb1af288
shukawam <shukawam@gmail.com>
2025-03-11 15:58:05 +0900

    Fixed apm tier

--- oci/ap-tokyo-1/apm.tf
+++ oci/ap-tokyo-1/apm.tf
@@ -1,6 +1,6 @@
 resource "oci_apm_apm_domain" "apm" {
   compartment_id = var.compartment_ocid
   display_name   = format("%s-apm-domain", var.prefix)
-  is_free_tier   = true
+  is_free_tier   = false
   freeform_tags  = local.freeform_tags
 }
