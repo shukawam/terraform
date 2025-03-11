5493ea2a1435bf602c7756a32d122f89690c009a
shukawam <shukawam@gmail.com>
2025-03-11 14:56:05 +0900

    Removed unused resources

--- oci/ap-tokyo-1/api_gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "oci_apigateway_gateway" "api_gateway" {
-  compartment_id = var.compartment_ocid
-  endpoint_type  = "PUBLIC"
-  subnet_id      = oci_core_subnet.public_subnet.id
-  display_name   = format("%s-api-gateway", var.prefix)
-  freeform_tags  = local.freeform_tags
-}
